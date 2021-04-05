034ba0182620377fc50b9db55dbfbd9df1a38155
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-04-04 22:38:08 -0400

    logout button hide/show implemented

--- react-client/src/components/Sidebar.js
+++ react-client/src/components/Sidebar.js
@@ -3,15 +3,16 @@ import "../App.css";
 import {SidebarData} from "./SidebarData"
 
 function sidebar() {
-    
+
     const screen = localStorage.getItem("screen");
     return (
         <div className="SideBar">
             <ul className="sidebarList">
             {SidebarData.map((val, key) => {
                 if (
-                  screen === val.accessiblity ||
-                  val.accessiblity === "auth"
+                  (screen === val.accessiblity ||
+                    val.accessiblity === "auth") &&
+                  val.accessiblity !== "logout"
                 ) {
                   return (
                     <li
@@ -30,9 +31,26 @@ function sidebar() {
                       <div id="title">{val.title}</div>
                     </li>
                   );
+                } else if((
+                    val.accessiblity === "logout" && (screen === "nurse" || screen === "patient")) ) {
+                     return (
+                       <li
+                         key={key}
+                         onClick={() => {
+                           window.location.pathname = val.link;
+                         }}
+                         className="row"
+                         id={
+                           window.location.pathname === val.link
+                             ? "active"
+                             : "passive"
+                         }
+                       >
+                         <div id="icon">{val.icon}</div>{" "}
+                         <div id="title">{val.title}</div>
+                       </li>
+                     );
                 }
-                
-
             })}
             </ul>
         </div>
--- react-client/src/components/SidebarData.js
+++ react-client/src/components/SidebarData.js
@@ -29,7 +29,7 @@ export const SidebarData = [
     title: "Dashboard",
     icon: <DashboardIcon />,
     link: "/dashboard",
-    accessiblity: "student",
+    accessiblity: "patient",
   },
   {
     title: "Friends",
@@ -41,7 +41,7 @@ export const SidebarData = [
     title: "Logout",
     icon: <PowerSettingsNewIcon />,
     link: "/logout",
-    accessiblity: "auth",
+    accessiblity: "logout",
   },
 ];
 
