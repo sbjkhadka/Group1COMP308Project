12cae18290d71f83914c8597c1e881ecab84227c
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-04-05 05:53:07 -0400

    Under construction screens prepared

---
+++ react-client/src/components/SelfAssessment.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SelfAssessment() {
+    return (
+      <div>
+        <img src="https://www.pngitem.com/pimgs/m/643-6437843_page-under-construction-oops-we-are-working-hd.png" /><br />
+        Use AI to predict the possible cause of health problem
+      </div>
+    );
+}
