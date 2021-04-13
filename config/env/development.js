435223d1c15878858545f40a3198985c18813187
Ayhan <abedirhan@hotmail.com>
2021-04-13 18:55:37 -0400

    Changed Db

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 //Development configuration options
 module.exports = {
   //To sign the session identifier, use a secret string
-  db: "mongodb://localhost/final-project",
+  db: "mongodb+srv://ayhansaglam:ayhansaglam123@ayhansaglam.m96z7.mongodb.net/final-project?retryWrites=true&w=majority",
   sessionSecret: "developmentSessionSecret",
   secretKey:
     "cc337b5487e414597578da43fdb78464c1a2e2d0fce8989816170316564e9b41dcc79168c61da10a41df163c1e1b4d6af122477229d0a7edfd6ffaf277670663",
