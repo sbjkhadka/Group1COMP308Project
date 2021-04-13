22a2a9fbb28bdbafe85b2d506522d35c1337b544
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-04-13 02:58:23 -0400

    Added content for nurses home page

---
+++ react-client/src/components/useFulResource.js
@@ -0,0 +1,45 @@
+export const ResourceList = [
+  {
+    title: "CNO",
+    link: "http://www.cno.org",
+  },
+  {
+    title: "Practice Standard",
+    link: "https://www.cno.org/globalassets/docs/prac/41001_documentation.pdf",
+  },
+  {
+    title: "Nurse Buff",
+    link: "https://www.nursebuff.com/new-nurse-advice/",
+  },
+  {
+    title: "Time Management",
+    link:
+      "https://www.youtube.com/watch?v=8vjrlriT-SE&ab_channel=RegisteredNurseRN",
+  },
+  {
+    title: "Graduates",
+    link:
+      "https://www.youtube.com/watch?v=Kn_n9HvXxWQ&ab_channel=RegisteredNurseRN&loop=0",
+  },
+  {
+    title: "Tips",
+    link:
+      "http://nursing.buffalo.edu/news-events/nurses-report.host.html/content/shared/nursing/articles/nurses-report/posts/new-nurse-tips.detail.html",
+  },
+  {
+    title: "Point of Care",
+    link: "https://rnao.ca/sites/rnao-ca/files/POC_16.5_x_8.5_WEB_0.pdf",
+  },
+  {
+    title: "TV Series in Wiki",
+    link: "https://en.wikipedia.org/wiki/Nurses_(Canadian_TV_series)",
+  },
+  {
+    title: "ONA",
+    link: "https://www.ona.org/",
+  },
+  {
+    title: "Canadian Nurses Association",
+    link: "https://www.cna-aiic.ca/en/news-room/events/national-nursing-week",
+  },
+];
